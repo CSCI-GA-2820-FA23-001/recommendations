da247add58c9e8e2eb00b17a100b6a614477bbca
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-10-11 18:55:56 -0400

    create db schema

--- service/models.py
+++ service/models.py
@@ -1,10 +1,14 @@
 """
-Models for YourResourceModel
+Models for Recommendation
 
 All of the models are stored in this module
 """
+import datetime
 import logging
+from enum import Enum
 from flask_sqlalchemy import SQLAlchemy
+
+
 
 logger = logging.getLogger("flask.app")
 
@@ -15,30 +19,52 @@ db = SQLAlchemy()
 # Function to initialize the database
 def init_db(app):
     """ Initializes the SQLAlchemy app """
-    YourResourceModel.init_db(app)
+    Recommendation.init_db(app)
 
 
 class DataValidationError(Exception):
     """ Used for an data validation errors when deserializing """
 
+class RecommendationType(Enum):
+    """ Enumeration of recommendation type """
+    UP_SELL = 1
+    CROSS_SELL = 2
+    ACCESSORY = 3
 
-class YourResourceModel(db.Model):
+class RecommendationStatus(Enum):
+    "" "Enumeration of recommendation status """
+    VALID = "valid"
+    OUT_OF_STOCK = "out of stock"
+    DEPRECATED = "deprecated"
+
+class Recommendation(db.Model):
     """
-    Class that represents a YourResourceModel
+    Class that represents a Recommendation
     """
 
     app = None
+    __tablename__ = "recommendation"
 
     # Table Schema
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(63))
-
+    recommendation_id = db.Column(db.Integer, primary_key=True)
+    source_item_id = db.Column(db.Integer, nullable=False)
+    target_item_id = db.Column(db.Integer, nullable=False)
+    recommendation_type = db.Column(
+        db.Enum(RecommendationType), server_default=(RecommendationType.UP_SELL.name)
+    )
+    recommendation_weight = db.Column(db.Float, nullable=False, default=0.0)
+    status = db.Column(
+        db.Enum(RecommendationStatus), nullable=False, server_default=RecommendationStatus.VALID.value
+    )
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    updated_at = db.Column(db.DateTime, onupdate=datetime.datetime.utcnow)
+ 
     def __repr__(self):
-        return f"<YourResourceModel {self.name} id=[{self.id}]>"
+        return f"<Recommendation {self.name} id=[{self.id}]>"
 
     def create(self):
         """
-        Creates a YourResourceModel to the database
+        Creates a Recommendation to the database
         """
         logger.info("Creating %s", self.name)
         self.id = None  # pylint: disable=invalid-name
@@ -47,24 +73,24 @@ class YourResourceModel(db.Model):
 
     def update(self):
         """
-        Updates a YourResourceModel to the database
+        Updates a Recommendation to the database
         """
         logger.info("Saving %s", self.name)
         db.session.commit()
 
     def delete(self):
-        """ Removes a YourResourceModel from the data store """
+        """ Removes a Recommendation from the data store """
         logger.info("Deleting %s", self.name)
         db.session.delete(self)
         db.session.commit()
 
     def serialize(self):
-        """ Serializes a YourResourceModel into a dictionary """
+        """ Serializes a Recommendation into a dictionary """
         return {"id": self.id, "name": self.name}
 
     def deserialize(self, data):
         """
-        Deserializes a YourResourceModel from a dictionary
+        Deserializes a Recommendation from a dictionary
 
         Args:
             data (dict): A dictionary containing the resource data
@@ -73,11 +99,11 @@ class YourResourceModel(db.Model):
             self.name = data["name"]
         except KeyError as error:
             raise DataValidationError(
-                "Invalid YourResourceModel: missing " + error.args[0]
+                "Invalid Recommendation: missing " + error.args[0]
             ) from error
         except TypeError as error:
             raise DataValidationError(
-                "Invalid YourResourceModel: body of request contained bad or no data - "
+                "Invalid Recommendation: body of request contained bad or no data - "
                 "Error message: " + error
             ) from error
         return self
@@ -94,22 +120,22 @@ class YourResourceModel(db.Model):
 
     @classmethod
     def all(cls):
-        """ Returns all of the YourResourceModels in the database """
-        logger.info("Processing all YourResourceModels")
+        """ Returns all of the Recommendation in the database """
+        logger.info("Processing all Recommendation")
         return cls.query.all()
 
     @classmethod
     def find(cls, by_id):
-        """ Finds a YourResourceModel by it's ID """
+        """ Finds a Recommendation by it's ID """
         logger.info("Processing lookup for id %s ...", by_id)
         return cls.query.get(by_id)
 
     @classmethod
     def find_by_name(cls, name):
-        """Returns all YourResourceModels with the given name
+        """Returns all Recommendation with the given name
 
         Args:
-            name (string): the name of the YourResourceModels you want to match
+            name (string): the name of the Recommendation you want to match
         """
         logger.info("Processing name query for %s ...", name)
         return cls.query.filter(cls.name == name)
