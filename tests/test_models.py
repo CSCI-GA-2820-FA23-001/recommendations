6ad6dc81d361419c7e8e7c89836c5d4838c1f494
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-10-11 21:25:35 -0400

    add 1 database schema/create test

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,38 +1,62 @@
 """
-Test cases for YourResourceModel Model
+Test cases for Recommendation Model
 
 """
 import os
 import logging
 import unittest
-from service.models import YourResourceModel, DataValidationError, db
+from service.models import Recommendation, DataValidationError, db, RecommendationStatus, RecommendationType
 
+DATABASE_URI = os.getenv(
+    "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/testdb"
+)
 
 ######################################################################
-#  YourResourceModel   M O D E L   T E S T   C A S E S
+#  Recommendation   M O D E L   T E S T   C A S E S
 ######################################################################
 # pylint: disable=too-many-public-methods
-class TestYourResourceModel(unittest.TestCase):
-    """ Test Cases for YourResourceModel Model """
+
+
+class TestRecommendation(unittest.TestCase):
+    """ Test Cases for Recommendation Model """
 
     @classmethod
     def setUpClass(cls):
         """ This runs once before the entire test suite """
+        pass
 
     @classmethod
     def tearDownClass(cls):
         """ This runs once after the entire test suite """
+        pass
 
     def setUp(self):
         """ This runs before each test """
+        db.drop_all()  # remove all data from previous tests
+        db.create_all()  # create tables for our data models
 
     def tearDown(self):
         """ This runs after each test """
+        db.session.remove()  # clean up the session
+        db.drop_all()        # tear down the database table
 
     ######################################################################
     #  T E S T   C A S E S
     ######################################################################
 
-    def test_example_replace_this(self):
-        """ It should always be true """
-        self.assertTrue(True)
+    def test_create_recommendation(self):
+        """ Create a recommendation and assert that it exists """
+        recommendation = Recommendation(
+            source_item_id=123,
+            target_item_id=456,
+            recommendation_type=RecommendationType.UP_SELL,
+            recommendation_weight=0.8,
+            status=RecommendationStatus.VALID
+        )
+        self.assertTrue(recommendation is not None)
+        self.assertEqual(recommendation.recommendation_id, None)
+        self.assertEqual(recommendation.source_item_id, 123)
+        self.assertEqual(recommendation.target_item_id, 456)
+        self.assertEqual(recommendation.recommendation_type, RecommendationType.UP_SELL.name)
+        self.assertEqual(recommendation.recommendation_weight, 0.8)
+        self.assertEqual(recommendation.status, RecommendationStatus.VALID.name)
