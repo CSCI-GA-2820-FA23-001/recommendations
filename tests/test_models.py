49e94bc386f8f83f5a0f4f331b9959f83a50988b
WYB929 <yw4145@nyu.edu>
2023-10-15 04:11:02 +0000

    add test case for read recommendations

--- tests/test_models.py
+++ tests/test_models.py
@@ -12,6 +12,7 @@ from service.models import (
     RecommendationStatus,
     RecommendationType,
 )
+from tests.factories import RecommendationFactory
 
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/testdb"
@@ -66,3 +67,57 @@ class TestRecommendation(unittest.TestCase):
         self.assertEqual(recommendation.recommendation_type, RecommendationType.UP_SELL)
         self.assertEqual(recommendation.recommendation_weight, 0.8)
         self.assertEqual(recommendation.status, RecommendationStatus.VALID)
+
+    def test_read_a_recommendation_by_id(self):
+        """It should Read a Recommendation by id"""
+        recommendation = RecommendationFactory()
+        logging.debug(recommendation)
+        recommendation.id = None
+        recommendation.create()
+        self.assertIsNotNone(recommendation.id)
+        # Fetch it back
+        found_recommendation = Recommendation.find(recommendation.id)
+
+        self.assertTrue(recommendation is not None)
+        self.assertEqual(found_recommendation.id, recommendation.id)
+        self.assertEqual(
+            found_recommendation.source_item_id, recommendation.source_item_id
+        )
+        self.assertEqual(
+            found_recommendation.target_item_id, recommendation.target_item_id
+        )
+        self.assertEqual(
+            found_recommendation.recommendation_type, recommendation.recommendation_type
+        )
+        self.assertEqual(
+            found_recommendation.recommendation_weight,
+            recommendation.recommendation_weight,
+        )
+        self.assertEqual(found_recommendation.status, recommendation.status)
+
+    def test_read_a_recommendation_by_source_item_id(self):
+        """It should Read a Recommendation by id"""
+        recommendation = RecommendationFactory()
+        logging.debug(recommendation)
+        recommendation.source_item_id = None
+        recommendation.create()
+        self.assertIsNotNone(recommendation.source_item_id)
+        # Fetch it back
+        found_recommendation = Recommendation.find_by_source_item_id(recommendation.id)
+
+        self.assertTrue(recommendation is not None)
+        self.assertEqual(found_recommendation.id, recommendation.id)
+        self.assertEqual(
+            found_recommendation.source_item_id, recommendation.source_item_id
+        )
+        self.assertEqual(
+            found_recommendation.target_item_id, recommendation.target_item_id
+        )
+        self.assertEqual(
+            found_recommendation.recommendation_type, recommendation.recommendation_type
+        )
+        self.assertEqual(
+            found_recommendation.recommendation_weight,
+            recommendation.recommendation_weight,
+        )
+        self.assertEqual(found_recommendation.status, recommendation.status)
